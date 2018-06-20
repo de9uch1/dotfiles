5c166fe94d413592fa70b8f0d147136f0e663fef
Hiroyuki Deguchi <d@dlambda.org>
2018-06-21 07:32:59 +0900

    update

--- install.sh
+++ install.sh
@@ -1,23 +1,41 @@
 #!/bin/sh
 
-if [ `which git` ] ; then
-    if [ `ssh -T git@github.com` ] ; then
-        git clone git@github.com:dlambda/dotfiles.git ~/.dotfiles
+DOTFILES_PATH="${HOME}/.dotfiles"
+
+case $(uname) in
+    Linux)
+        MAKE="make"
+        ;;
+    FreeBSD)
+        MAKE="gmake"
+        ;;
+    *)
+        
+esac
+
+get_git_repo() {
+    if [ $(ssh -fT git@github.com) ]; then
+        git clone git@github.com:dlambda/dotfiles.git $DOTFILES_PATH
     else
-        git clone https://github.com/dlambda/dotfiles.git ~/.dotfiles
+        git clone https://github.com/dlambda/dotfiles.git $DOTFILES_PATH
     fi
-elif [ `which wget` ] ; then
-    mkdir /tmp/dotfiles
-    cd /tmp/dotfiles
-    wget https://github.com/dlambda/dotfiles/archive/master.zip
-    unzip master.zip
-    mv dotfiles-master ~/.dotfiles
-    rm -r /tmp/dotfiles
+}
+
+get_master_archive() {
+    local SRC_URI="https://github.com/dlambda/dotfiles/archive/master.tar.gz"
+    if [ $(which curl) ]; then
+        curl -L $SRC_URI
+    elif [ $(which wget) ]; then
+        wget -O - $SRC_URI 
+    fi | tar xzv
+    mv dotfiles-master $DOTFILES_PATH
+}
+
+if [ $(which git) ]; then
+    get_git_repo
+else
+    get_master_archive
 fi
 
-cd ~/.dotfiles
-if [ $(uname) = "Linux" ] ; then
-    make link
-elif [ $(uname) = "FreeBSD" ] ; then
-    gmake link
-fi
+cd $DOTFILES_PATH
+$MAKE link

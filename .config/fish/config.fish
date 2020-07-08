f5280093e513c4b283106faab4915309b4dba4ca
Hiroyuki Deguchi <deguchi@ai.cs.ehime-u.ac.jp>
2020-07-08 16:08:35 +0900

    dracula-theme

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -8,11 +8,51 @@ if [ $TERM = "dumb" ] || [ $TERM = "eterm-color" ]
     exec bash
 end
 
-# powerline
-if [ -f $FISH_CONFIG_DIR/functions/powerline-setup.fish ]
-    powerline-setup
-    set -g theme_color_scheme solarized
+# Theme
+if ! [ -f $FISH_CONFIG_DIR/conf.d/dracula.fish ]
+    mkdir -p $FISH_CONFIG_DIR/conf.d
+    curl -o $FISH_CONFIG_DIR/conf.d/dracula.fish \
+        "https://raw.githubusercontent.com/dracula/fish/master/conf.d/dracula.fish"
 end
+set -g theme_display_git yes
+set -g theme_display_git_dirty no
+set -g theme_display_git_untracked no
+set -g theme_display_git_ahead_verbose yes
+set -g theme_display_git_dirty_verbose yes
+set -g theme_display_git_stashed_verbose yes
+set -g theme_display_git_master_branch yes
+set -g theme_git_worktree_support yes
+set -g theme_use_abbreviated_branch_name yes
+set -g theme_display_vagrant yes
+set -g theme_display_docker_machine no
+set -g theme_display_k8s_context yes
+set -g theme_display_hg yes
+set -g theme_display_virtualenv no
+set -g theme_display_nix no
+set -g theme_display_ruby no
+set -g theme_display_nvm yes
+set -g theme_display_user yes
+set -g theme_display_hostname yes
+set -g theme_display_vi no
+set -g theme_display_date yes
+set -g theme_display_cmd_duration yes
+set -g theme_title_display_process yes
+set -g theme_title_display_path no
+set -g theme_title_display_user yes
+set -g theme_title_use_abbreviated_path no
+set -g theme_date_format "+%a %H:%M"
+set -g theme_date_timezone Asia/Tokyo
+set -g theme_avoid_ambiguous_glyphs no
+set -g theme_powerline_fonts yes
+set -g theme_nerd_fonts no
+set -g theme_show_exit_status yes
+set -g theme_display_jobs_verbose yes
+set -g default_user your_normal_user
+set -g theme_color_scheme dracula
+set -g fish_prompt_pwd_dir_length 0
+set -g theme_project_dir_lengtsh 1
+set -g theme_newline_cursor yes
+set -g theme_newline_prompt ' '
 
 # setup fisherman
 if not [ -f $FISH_CONFIG_DIR/functions/fisher.fish ]
@@ -29,7 +69,7 @@ set -gx GHQ_ROOT $HOME/src
 set -gx GOPATH $HOME/.go
 function add_path
     set -l maybe_path $argv[1]
-    if not contains $maybe_path $PATH && [ -d $maybe_path ]
+    if not contains $maybe_path $PATH
         set -gx PATH $maybe_path $PATH
     end
 end

b6e1f008a33f1069871e5417db45fc3b4d945842
Hiroyuki Deguchi <deguchi@ai.cs.ehime-u.ac.jp>
2020-07-19 16:36:43 +0900

    fix install.sh

--- install.sh
+++ install.sh
@@ -5,6 +5,7 @@ SRC_URI_GIT_SSH="git@github.com:de9uch1/dotfiles.git"
 SRC_URI_GIT_HTTP="https://github.com/de9uch1/dotfiles.git"
 SRC_URI_TARBALL="https://github.com/de9uch1/dotfiles/archive/master.tar.gz"
 
+MAKE=""
 case $(uname) in
     Linux)
         MAKE="make"
@@ -13,11 +14,15 @@ case $(uname) in
         MAKE="gmake"
         ;;
     *)
-        
+        ;;
 esac
+if [ -z "$MAKE" ]; then
+    echo "GNUmake not found, abort."
+    exit -1
+fi
 
 get_git_repo() {
-    if [ $(ssh -fT git@github.com) ]; then
+    if ssh -T git@github.com; [ $? -eq 1 ]; then
         git clone $SRC_URI_GIT_SSH $DOTFILES_PATH
     else
         git clone $SRC_URI_GIT_HTTP $DOTFILES_PATH
@@ -25,16 +30,15 @@ get_git_repo() {
 }
 
 get_master_archive() {
-    local 
-    if $(which curl >/dev/null 2>&1) ; then
+    if command -v curl >/dev/null; then
         curl -L $SRC_URI_TARBALL
-    elif $(which wget >/dev/null 2>&1) ; then
+    elif command -v wget >/dev/null; then
         wget -O - $SRC_URI_TARBALL
     fi | tar xzv
     mv dotfiles-master $DOTFILES_PATH
 }
 
-if $(which git >/dev/null 2>&1); then
+if command -v git >/dev/null; then
     get_git_repo
 else
     get_master_archive
@@ -42,3 +46,4 @@ fi
 
 cd $DOTFILES_PATH
 $MAKE link
+exec $SHELL

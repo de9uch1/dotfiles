e1346325609b87e1fe6f0ef0726fd097f0d8af5f
Hiroyuki Deguchi <deguchi@ai.cs.ehime-u.ac.jp>
2021-04-28 15:00:56 +0900

    bug fix

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -82,29 +82,27 @@ function dots
     end
 end
 
-# for Gentoo
+# for Gentoo system
 if [ "{$DISTRIB_ID}x" = "gentoox" ]
-    function gentoo-mode
-        # app-portage/portage-utils
-        alias lastsync 'qlop -s | tail -n1'
-        alias qtime 'qlop -tv'
+    # app-portage/portage-utils
+    alias lastsync 'qlop -s | tail -n1'
+    alias qtime 'qlop -tv'
 
-        # app-portage/flaggie
-        function acckw
-            switch $argv[1]
-                case a
-                    sudo flaggie $argv[2] '+kw::~amd64'
-                case d
-                    sudo flaggie $argv[2] '-kw::~amd64'
-                case r
-                    sudo flaggie $argv[2] '%kw::~amd64'
-                case v
-                    sudo flaggie $argv[2] '?kw::~amd64'
-                case ''
-                    return 1
-                case '*'
-                    return 1
-            end
+    # app-portage/flaggie
+    function acckw
+        switch $argv[1]
+            case a
+                sudo flaggie $argv[2] '+kw::~amd64'
+            case d
+                sudo flaggie $argv[2] '-kw::~amd64'
+            case r
+                sudo flaggie $argv[2] '%kw::~amd64'
+            case v
+                sudo flaggie $argv[2] '?kw::~amd64'
+            case ''
+                return 1
+            case '*'
+                return 1
         end
     end
 end

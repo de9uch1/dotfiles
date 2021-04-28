18bc124794a3abfade05c829ba66290ee1d85fd5
Hiroyuki Deguchi <deguchi@ai.cs.ehime-u.ac.jp>
2021-04-28 14:53:55 +0900

    Remove unnecessary files

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -84,7 +84,29 @@ end
 
 # for Gentoo
 if [ "{$DISTRIB_ID}x" = "gentoox" ]
-    gentoo-mode
+    function gentoo-mode
+        # app-portage/portage-utils
+        alias lastsync 'qlop -s | tail -n1'
+        alias qtime 'qlop -tv'
+
+        # app-portage/flaggie
+        function acckw
+            switch $argv[1]
+                case a
+                    sudo flaggie $argv[2] '+kw::~amd64'
+                case d
+                    sudo flaggie $argv[2] '-kw::~amd64'
+                case r
+                    sudo flaggie $argv[2] '%kw::~amd64'
+                case v
+                    sudo flaggie $argv[2] '?kw::~amd64'
+                case ''
+                    return 1
+                case '*'
+                    return 1
+            end
+        end
+    end
 end
 
 # Python
